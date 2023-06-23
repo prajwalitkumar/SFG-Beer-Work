9b77449a78207329a8961cde3a978fd068691b84
prajwalitkumar <prajwalitkumar007@gmail.com>
2023-06-23 14:47:22 +0530

    Updating the Base project

---
+++ src/test/java/com/sfgbeerwork/BeerOrder/BeerOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfgbeerwork.BeerOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
